1eabb70185dd6b914596c9599ea572a083fb6183
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2024-01-09 18:47:40 +0530

    Create Postorder.cpp

---
+++ Postorder.cpp
@@ -0,0 +1,18 @@
+void post(Node*root,vector<int> &v)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    post(root->left,v);
+    post(root->right,v);
+    v.push_back(root->data);
+}
+vector <int> postOrder(Node* root)
+{
+  // Your code here
+  vector<int> v;
+  post(root,v);
+  return v;
+  
+}
