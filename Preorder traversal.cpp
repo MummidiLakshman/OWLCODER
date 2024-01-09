2916ca4a14931378170cea9aea5ccf84ad16c36c
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2024-01-09 18:46:07 +0530

    Create Preorder traversal.cpp

---
+++ Preorder traversal.cpp
@@ -0,0 +1,18 @@
+void pre(Node* root, vector<int> &v)
+{
+    if(root==NULL)
+    {
+        return ;
+    }
+    v.push_back(root->data);
+    pre(root->left, v);
+    pre(root->right, v);
+}
+vector <int> preorder(Node* root)
+{
+  // Your code here
+  vector<int> v;
+  pre(root,v);
+  return v;
+  
+}
