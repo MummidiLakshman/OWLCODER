9adf7fe0c470c8428b597b1d77c88f9913e01e00
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2024-01-09 18:44:56 +0530

    Create Inorder Traversal.cpp

---
+++ Inorder Traversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> v;
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        
+        if(root==NULL)
+        {
+            return v;
+        }
+        inOrder(root->left);
+        v.push_back(root->data);
+        inOrder(root->right);
+        return v;
+    }
+};
