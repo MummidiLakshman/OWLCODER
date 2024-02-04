4ae50df5ef47d7bad6983806c08126b1e349e5dd
MummidiLakshman <96734843+MummidiLakshman@users.noreply.github.com>
2024-02-04 22:55:02 +0530

    Create Binary tree level order.cpp

---
+++ Binary tree level order.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void Fun(TreeNode* root, vector<vector<int>> &vec, int i)
+    {
+        if(root==NULL) return;
+        if(i>=vec.size()) vec.push_back({root->val});
+        else vec[i].push_back(root->val);
+        Fun(root->left, vec, i+1);
+        Fun(root->right, vec, i+1);
+    }
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> vec;
+        Fun(root, vec, 0);
+        reverse(vec.begin(), vec.end());+
+        return vec;
+    }
+};
